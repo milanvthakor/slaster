0e94b8c7624b72aef9c4cd3a1e6b121b74b84998
Milan Thakor <milanv.thakor@gmail.com>
2025-06-23 21:10:16 +0530

    Change the font and update home page.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
     <div style={{ textAlign: "center" }}>
-      <h2>Welcome to Slaster!</h2>
+      <h2 style={{ color: "var(--accent-color)" }}>Welcome to Slaster!</h2>
+      <p style={{ fontSize: "1.3rem" }}>
+        <b>Slaster</b> seamlessly connects your Google Calendar and Slack,
+        letting you set custom status messages, emojis, and Do Not Disturb
+        preferences directly from calendar events. It's a secure, <b>no-cost</b>{" "}
+        solution that keeps your Slack presence in sync — automatically and in
+        real time.
+      </p>
     </div>
   );
 }
